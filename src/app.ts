f7515c80079650e751833f166a9f7eef1a2249c9
Henrry Esposito <henrrygesposito@gmail.com>
2021-10-20 23:29:46 -0300

    Github oauth get user data

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import "dotenv/config";
 import express from "express";
+import { router } from "./routes";
 
 const app = express();
+
+
+app.use(express.json());
+app.use(router);
+
 
 app.get("/github", (request, response) => {
    response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
@@ -12,5 +18,6 @@ app.get("/signin/callback", (request, response) => {
 
     return response.json(code);
 })
+
 
 app.listen(4000, () => console.log('* Server us rinning on PORT 4000'));
