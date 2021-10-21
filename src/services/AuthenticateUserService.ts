2f83223d334ad5d23f2b2cda4d889b1debcdefff
Henrry Esposito <henrrygesposito@gmail.com>
2021-10-21 18:47:58 -0300

    create user

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,5 @@
-import "axios";
 import axios from "axios";
+import prismaClient from "../prisma"
 
 interface IAccessTokenResponse {
     access_token: string
@@ -32,6 +32,25 @@ class AuthenticateUserService {
                 authorization: `Bearer ${accessTokenResponse.access_token}`
             }
         })
+
+        const { login, id, avatar_url, name } = response.data
+
+        let user = await prismaClient.user.findFirst({
+           where: {
+               github_id: id
+           } 
+        })
+
+        if (!user) {
+            await prismaClient.user.create({
+                data: {
+                    github_id: id,
+                    login,
+                    avatar_url,
+                    name
+                }
+            })
+        }
 
         return response.data;
     }
